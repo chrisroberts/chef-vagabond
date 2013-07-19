ece5e5f5136c56c1a7e43a71315eb31c8b653728
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-19 07:49:32 -0700

    Remove release minors from centos. Add attributes for servers.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,9 +10,17 @@ default[:vagabond][:bases][:centos_63][:template] = 'centos'
 default[:vagabond][:bases][:centos_63][:template_options] = {'--release' => '6', '--releaseminor' => '3'}
 default[:vagabond][:bases][:centos_64][:template] = 'centos'
 default[:vagabond][:bases][:centos_64][:template_options] = {'--release' => '6', '--releaseminor' => '4'}
+default[:vagabond][:bases][:centos_5][:template] = 'centos'
+default[:vagabond][:bases][:centos_5][:template_options] = {'--release' => '5'}
+default[:vagabond][:bases][:centos_6][:template] = 'centos'
+default[:vagabond][:bases][:centos_6][:template_options] = {'--release' => '6'}
 default[:vagabond][:bases][:debian_6][:template] = 'debian'
 default[:vagabond][:bases][:debian_6][:create_environment] = {'SUITE' => 'squeeze'}
 default[:vagabond][:bases][:debian_7][:template] = 'debian'
 default[:vagabond][:bases][:debian_7][:create_environment] = {'SUITE' => 'wheezy'}
 default[:vagabond][:customs] = {}
-default[:vagabond][:server_base] = true
+default[:vagabond][:server][:erchefs] = ['11.0.8'] # versions
+default[:vagabond][:server][:base] = 'ubuntu_1204'
+default[:vagabond][:server][:prefix] = 'vb-server-'
+default[:vagabond][:server][:zero_lxc_name] = 'vb-zero-server'
+default[:vagabond][:host_cookbook_store] = Chef::Config[:cookbook_path]
