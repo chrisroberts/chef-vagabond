cb16a0ff1cbb80f1cb908693acb486f4d96b0763
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-13 08:08:23 -0700

    Update dependency restrictions. Bump version for release v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'vagabond'
-version '1.0.1'
+version '1.0.2'
 license 'Apache 2.0'
 maintainer 'Chris Roberts'
 maintainer_email 'chrisroberts.code@gmail.com'
 
-depends 'lxc', '~> 1.0.1'
-depends 'apt', '~> 2.0.0'
+depends 'lxc', '~> 1.1.0'
+depends 'apt', '>= 2.0.0'
